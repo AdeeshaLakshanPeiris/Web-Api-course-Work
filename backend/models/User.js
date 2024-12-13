17780f1325b8f037e59d6e18cab67f0aa46ccc09
AdeeshaLakshanPeiris <adeeshapiris@gmail.com>
2024-12-14 00:20:39 +0530

    improve authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'driver', 'passenger'], required: true },
+    telephone_no: { type: String, required: false, unique: true },
+    role: { type: String, enum: ['admin', 'driver', 'passenger'], required: true, default: 'passenger' },
 });
 
 module.exports = mongoose.model('User', userSchema);
