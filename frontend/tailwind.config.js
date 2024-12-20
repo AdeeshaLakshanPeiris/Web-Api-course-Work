a5ffc452b46acaee27bc11956fedf0ab55dcb628
AdeeshaLakshanPeiris <adeeshapiris@gmail.com>
2024-12-20 16:43:26 +0530

    update2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    'node_modules/preline/dist/*.js',
-],
-theme: {
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
     extend: {},
-},
-plugins: [
-  require('preline/plugin'),
-],
-}
-
+  },
+  plugins: [
+    require("preline/plugin"),
+  ],
+};
