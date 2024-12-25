3e9080058d831ff39bd472572abf1cdb5470528a
AdeeshaLakshanPeiris <adeeshapiris@gmail.com>
2024-12-25 23:46:38 +0530

    implement feedback services

--- frontend/src/components/contact_us/ContactUs.jsx
+++ frontend/src/components/contact_us/ContactUs.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import api from "../../api/api";
+import api from "../../api/feedback_api.jsx";
 
 const ContactUs = () => {
     const [formData, setFormData] = useState({
